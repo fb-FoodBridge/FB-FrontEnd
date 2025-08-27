a1b48e8bb6ba40d9178e50103115081898e24951
xsalles <xs.salles@gmail.com>
2025-08-27 14:20:28 -0300

    refactored: added flex wrap to avoid erros of responsiveness

--- src/template/LandingPage/FaqSection/index.tsx
+++ src/template/LandingPage/FaqSection/index.tsx
@@ -9,7 +9,7 @@ export default function FaqSection() {
       className="flex flex-col gap-10 items-center max-w-[1440px] w-full xl:px-12 xl:py-8 py-16 px-4 md:px-6"
     >
       <h3 className="font-medium text-5xl text-center">Perguntas Frequentes</h3>
-      <div className="flex gap-4">
+      <div className="flex flex-wrap gap-4 items-center justify-center">
         {faqSectionData.categorys.map((category, key) => (
           <OptionCategory key={key} {...category} />
         ))}
